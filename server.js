e189345e2226f739f0c28244014ec2ab2ce2ade6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-26 19:04:13 -0400

    Add login route to server.js and update navbar.ejs

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 
 const landingRoute = require('./routes/landing');
+const loginRoute = require('./routes/login');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -42,6 +43,7 @@ app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 
 app.use('/landing', landingRoute)
+app.use('/login', loginRoute);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
