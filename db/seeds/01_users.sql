d28118093038d0a2bb1f7935d24b883d58c9c34b
Rebecca <rebecca90469@gmail.com>
2024-04-28 17:47:14 -0400

    Data for users table mostly created/formatted

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,20 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (
+  username, email, password, user_icon, story_id, contribution_id) 
+  VALUES (
+    'levibunny', 'levi@bunny', 'password1', 'rabbit.png', <story_id>, <contribution_id>);
+INSERT INTO users (
+  username, email, password, user_icon, story_id, contribution_id) 
+  VALUES (
+    'laurelbunny', 'laurel@bunny', 'password2', 'rabbit.png', <story_id>, <contribution_id>);
+INSERT INTO users (
+  username, email, password, user_icon, story_id, contribution_id) 
+  VALUES (
+    'dexterkitty', 'dexter@kitty', 'password3', 'cat.png', <story_id>, <contribution_id>);
+INSERT INTO users (
+  username, email, password, user_icon, story_id, contribution_id) 
+  VALUES (
+    'faunaAC', 'fauna@ac', 'password4', 'deer.png', <story_id>, <contribution_id>);
+INSERT INTO users (
+  username, email, password, user_icon, story_id, contribution_id) 
+  VALUES (
+    'patchesAC', 'patches@ac', 'password5', 'panda.png', <story_id>, <contribution_id>);
