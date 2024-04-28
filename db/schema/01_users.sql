9af4da660de7cfbd81d5c7fd67d6bae6e9a7f061
Rebecca <rebecca90469@gmail.com>
2024-04-28 16:07:02 -0400

    Start CREATE TABLES for users, stories and contributions

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,10 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(32) NOT NULL,
+  email VARCHAR(32) NOT NULL,
+  password VARCHAR(32) NOT NULL,
+  user_icon VARCHAR (255),
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
+  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
 );
---
+++ db/schema/02_stories.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS stories CASCADE;
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
+  title VARCHAR (72) NOT NULL,
+  content TEXT NOT NULL, 
+  created_date DATETIME,
+  is_complete BOOLEAN NOT NULL DEFAULT FALSE 
+);
