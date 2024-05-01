295a687df6f6123d101718e591a32d9bceabc974
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-01 15:05:11 -0400

    Refactor user profile routes and add user ID middleware

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -1,61 +1,67 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const router  = express.Router();
+const router = express.Router();
 const { getUserById } = require('../db/queries/users');
 const { getContributionsByUserId } = require('../db/queries/contributions');
+const { getRecentStories } = require('../db/queries/stories');
 
 // Middleware to extract user ID from URL parameters and set it in cookies
 router.param('id', (req, res, next, id) => {
   res.cookie('user_id', id);
   next();
 });
-const { getRecentStories } = require('../db/queries/stories');
 
 router.get('/:id', (req, res) => {
-  let id = req.cookies["user_id"]
-  Promise.all ([
+  const id = req.cookies["user_id"];
+  Promise.all([
     getUserById(id),
-    getRecentStories() 
+    getRecentStories()
   ])
-  .then(results => {
-    const user = results[0][0];
-    const stories = results[1];
-  res.render('user_profile', {user, stories, user_id: id});
-  })
-}); 
-
-// Route for user contributions
-router.get('/:id/contributions', (req, res) => {
-  let id = req.params.id;
-  Promise.all([getContributionsByUserId(id), getUserById(id)])
-    .then(([contributions, user]) => {
-   
-      console.log("This is the user:",user);
-      console.log("This is the contribution:",contributions);
-      
-      res.render('user_contributions', { user_id: id, listOfContributions: contributions, user });
+    .then(results => {
+      const user = results[0][0];
+      const stories = results[1];
+      res.render('user_profile', { user, stories, user_id: id });
     })
     .catch(error => {
-      // Handle errors
       console.error('Error:', error);
       res.status(500).send('Internal Server Error');
     });
-}); 
+});
+
+// Route for user contributions
+router.get('/:id/contributions', (req, res) => {
+  const id = req.params.id;
+  Promise.all([getContributionsByUserId(id), getUserById(id)])
+    .then(([contributions, user]) => {
+      console.log("This is the user:", user);
+      console.log("This is the contribution:", contributions);
+      res.render('user_contributions', { user_id: id, listOfContributions: contributions, user });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
 
 // Route for user stories
 router.get('/:id/stories', (req, res) => {
-  res.render('user_stories');
-}); 
+  const id = req.params.id;
+  res.render('user_stories', { user_id: id });
+});
 
-// Route to create a story for logged in user
+// Route to create a story for logged-in user
 router.get('/:id/new_story', (req, res) => {
-  let id = req.params.id;
+  const id = req.params.id;
   getUserById(id)
     .then(users => {
-      let user = users[0];
-      console.log(user)
+      const user = users[0];
+      console.log(user);
       res.render('new_story', { user_id: id, user });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
     });
-}); 
+});
 
 module.exports = router;
