02c7d48743ac048f43b37c53ae954ee75865c1b0
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-30 22:00:00 -0400

    Update getContributionsByUserId function to include user avatars in contributions.js

--- db/queries/contributions.js
+++ db/queries/contributions.js
@@ -11,7 +11,7 @@ const getContributionsByStoryId = (id) => {
 }
 const getContributionsByUserId = (id) => {
   return db.query(
-    `SELECT contributions.*, users.username AS username, stories.title AS story_title FROM contributions
+    `SELECT contributions.*, users.username AS username, users.user_icon AS avatar, stories.title AS story_title FROM contributions
        JOIN users ON users.id = user_id 
        JOIN stories ON stories.user_id = users.id
        WHERE contributions.user_id = $1;`, [id])
