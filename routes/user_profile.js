f0a146aab26b322a775c1db5d554d2d0756aa5fb
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-30 22:00:04 -0400

    Refactor user profile routes and add user ID middleware

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -4,44 +4,54 @@ const router  = express.Router();
 const { getUserById } = require('../db/queries/users');
 const { getContributionsByUserId } = require('../db/queries/contributions');
 
-//Route for specific logged in user profile
-router.get('/', (req, res) => {
-  let id = req.cookies["user_id"]
-  console.log(id)
+// Middleware to extract user ID from URL parameters and set it in cookies
+router.param('id', (req, res, next, id) => {
+  res.cookie('user_id', id);
+  next();
+});
+
+// Route for specific logged in user profile
+router.get('/:id', (req, res) => {
+  let id = req.params.id;
   getUserById(id)
-  .then(users => {
-    let user = users[0];
-    const templatevars = {
-      user
-    }
-    res.render('user_profile', templatevars);
-  });
+    .then(users => {
+      let user = users[0];
+      res.render('user_profile', { user_id: id, user });
+    });
 }); 
 
-//Route for user contributions
-router.get('/contributions', (req, res) => {
-
-  let id = req.cookies["user_id"]
-  
-  getContributionsByUserId(id)
-  .then(contributions => {
-    console.log(contributions)
-    const templatevars = { contributions }
-    res.render('user_contributions', templatevars);
-  })
+// Route for user contributions
+router.get('/:id/contributions', (req, res) => {
+  let id = req.params.id;
+  Promise.all([getContributionsByUserId(id), getUserById(id)])
+    .then(([contributions, user]) => {
+   
+      console.log("This is the user:",user);
+      console.log("This is the contribution:",contributions);
+      
+      res.render('user_contributions', { user_id: id, listOfContributions: contributions, user });
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    });
 }); 
 
-//Route for user stories
-router.get('/stories', (req, res) => {
+// Route for user stories
+router.get('/:id/stories', (req, res) => {
   res.render('user_stories');
 }); 
 
-//Route to create a story for logged in user
-router.get('/new_story', (req, res) => {
-  res.render('new_story');
+// Route to create a story for logged in user
+router.get('/:id/new_story', (req, res) => {
+  let id = req.params.id;
+  getUserById(id)
+    .then(users => {
+      let user = users[0];
+      console.log(user)
+      res.render('new_story', { user_id: id, user });
+    });
 }); 
-
-
-
 
 module.exports = router;
