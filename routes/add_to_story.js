27bb15aa7e179e5bb49c9630f0163bfc59fca79d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-02 22:52:46 -0400

    feat: Render user information on add_to_story page

--- routes/add_to_story.js
+++ routes/add_to_story.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router  = express.Router();
+const { getUserById } = require('../db/queries/users');
 
 router.get('/:id', (req, res) => {
-  let userId = req.cookies["user_id"]
+  let id = req.cookies["user_id"]
   const storyId = req.params.id // story id
-  res.render('add_to_story', {user_id: userId, storyId});
+  getUserById(id)
+  .then(result => {
+    const user = result[0]
+    console.log(user)
+    res.render('add_to_story', {user_id: id, storyId, user});
+  })
 });
 
 module.exports = router;
