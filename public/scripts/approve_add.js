53c05d8f00e12850361be0dc6040cb0d788e9368
Rebecca <rebecca90469@gmail.com>
2024-05-02 21:01:02 -0400

    Minor fixes

--- public/scripts/approve_add.js
+++ public/scripts/approve_add.js
@@ -1,11 +1,8 @@
-const approveStory = document.getElementsByClassName('complete-story');
-
 $(document).ready(()=> {
- $('.approve').on('click', (event) => {
+ $('.contribution_cards_container').on('click', '.approve', (event) => {
   const contribution_id = $(event.target).data('contribution_id');
   const story_id = $(event.target).data('story_id');
   const user_id = $(event.target).data('user_id');
-  // '/:id/pending/:story_id/:contribution_id/update_story'
   const route = `/user/${user_id}/pending/${story_id}/${contribution_id}/update_story`;
   $.post(route, (results) => {
     console.log("Ajax call complete", results);
