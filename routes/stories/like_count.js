0bbb9b5b9c36462d73ed73324c298053fb020e65
Rebecca <rebecca90469@gmail.com>
2024-06-11 17:19:22 -0400

    Mini change

--- routes/stories/like_count.js
+++ routes/stories/like_count.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { updateStoryLikes } = require('../../db/queries/stories');
 
-// Middleware to extract user ID from URL parameters and set it in cookies
-router.param('id', (req, res, next, id) => {
-  res.cookie('user_id', id);
-  next();
-});
-
 // Update a like count for a story
-router.post(':id/stories/:story_id', (req, res) => {
-  let id = req.params.id;
+router.post('/stories/:story_id', (req, res) => {
   const story_id = req.params.story_id;
   const upvote_count = req.body.upvote_count;
   console.log("ROUTER", story_id, upvote_count);
   updateStoryLikes(story_id, upvote_count)
     .then((results) => {
-      return res.redirect(`/stories/${story_id}`, {  user_id: id });
+      return res.redirect(`/stories/${story_id}`);
     })
     .catch(error => {
       res.status(500).send(error.message);
