5905f27eea26a9a3dd0b48621c2ddb6795e338bc
Rebecca <rebecca90469@gmail.com>
2024-06-11 20:32:35 -0400

    Like button functionality only to stories/show page

--- public/scripts/likebutton.js
+++ public/scripts/likebutton.js
@@ -1,20 +1,6 @@
 // Script for like button counter functionality
 // Like button and Like counter elements in HTML need to be together for this function to work properly
 
-// const likeButton = document.getElementsByClassName('like-button');
-// const likeCount = document.getElementsByClassName('like-count');
-// const container = document.getElementsByClassName('mainstory');
-
-// for (const likeButton of likeButtons) {
-//   let likeCount = 0;
-//   likeButton.addEventListener('click', function() {
-//     const likeCountElement = likeButton.nextElementSibling;
-//     likeCount = Number(likeCountElement.innerText);
-//     likeCount++;
-//     likeCountElement.innerText = likeCount;
-//   });
-// }
-
 $(document).ready(() => {
   $('.like-button').on('click', function (event) {
     event.preventDefault();
@@ -25,11 +11,6 @@ $(document).ready(() => {
 
     upvote_count++;
     likeCountElement.text(upvote_count);
-
-    console.log("This is the upvote count: ", upvote_count);
-    console.log("This is the story_id: ", storyId);
-
-    console.log("AJAX ONE");
     const route = `/stories/${storyId}`;
 
     $.ajax({
@@ -37,33 +18,10 @@ $(document).ready(() => {
       type: 'POST',
       data: { upvote_count: upvote_count },
       success: function (response) {
-        // Handle success if needed
-        console.log('Like count updated successfully');
       },
       error: function (xhr, status, error) {
-        // Handle error if needed
         console.error('Error updating like count:', error);
       }
     });
   });
 });
-
-// $.ajax({
-//   url: '/like/stories/' + story_id,
-//   type: 'POST',
-//   data: { upvote_count: upvote_count },
-//   success: function(response) {
-//       // Handle success if needed
-//       console.log('Like count updated successfully');
-//   },
-//   error: function(xhr, status, error) {
-//       // Handle error if needed
-//       console.error('Error updating like count:', error);
-//   }
-// });
-
-
-
-// $.post(route, { upvote_count, storyId }, (results) => {
-//   console.log("AJAX", results);
-// });
--- routes/stories/like_count.js
+++ routes/stories/like_count.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { updateStoryLikes } = require('../../db/queries/stories');
 
-// Update a like count for a story
+// Update a like count for a story in STORY SHOW
 router.post('/stories/:story_id', (req, res) => {
   const story_id = req.params.story_id;
   const upvote_count = req.body.upvote_count;
-  console.log("ROUTER", story_id, upvote_count);
   updateStoryLikes(story_id, upvote_count)
     .then((results) => {
       return res.redirect(`/stories/${story_id}`);
