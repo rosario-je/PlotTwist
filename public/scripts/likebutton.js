7916b74b08ab1605e7f7063a376394862cfa2cb2
Rebecca <rebecca90469@gmail.com>
2024-08-14 17:55:48 -0400

    Update contributions button

--- public/scripts/likebutton.js
+++ public/scripts/likebutton.js
@@ -25,3 +25,28 @@ $(document).ready(() => {
     });
   });
 });
+
+$(document).ready(() => {
+  $('.like-button-contribution').on('click', function (event) {
+    event.preventDefault();
+
+    let storyId = $(this).data('liked_story');
+    const likeCountElement = $(this).siblings('.like-count');
+    let upvote_count = parseInt(likeCountElement.text());
+
+    upvote_count++;
+    likeCountElement.text(upvote_count);
+    const route = `/stories/${storyId}`;
+
+    $.ajax({
+      url: '/like/stories/' + storyId,
+      type: 'POST',
+      data: { upvote_count: upvote_count },
+      success: function (response) {
+      },
+      error: function (xhr, status, error) {
+        console.error('Error updating like count:', error);
+      }
+    });
+  });
+});
